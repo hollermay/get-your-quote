2e1b674fc3122c789d32c3159998fc835e14a6f2
Udayan Sharma <122338009+hollermay@users.noreply.github.com>
2023-08-18 12:35:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ import { generateTextAndImage } from "./utils.js"
 let name = "Udayan Sharma"
 
 // 2. Change the value of the variable to your favorite activity
-let favoriteActivity = "Graphic Design"
+let favoriteActivity = "Soccer"
 
 // 3. Assign the favoritePlace variable your favorite place
 // I.e. city, mountain, pub, forrest, beach, Manhattan, etc.
-let favoritePlace = "New York"
+let favoritePlace = "France"
 
 // 4. Configure the AI by setting a temperature from 0 to 1
 // The higher temperature, the more random & experimental output
-let temperature = 1
+let temperature = 0.85
 
 // Optional: delete "avatar.jpg" and add a photo of yourself
 // (remember to use "avatar.jpg" as the name of your photo)
